4eac7e1ba68339520937627b093c624327bdde09
Deva <dev.palanisamy@gmail.com>
2015-05-30 23:33:02 +0100

    Added Source Files to Git

---
+++ Calculator/Calculator.Tests/Source/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using Calculator.Core;
+
+namespace Calculator.Tests
+{
+	public class CalculatorTests
+	{
+		SimpleCalculator calculator;
+
+		public CalculatorTests ()
+		{
+		}
+
+		[Test]
+		public void TestCalculatorForNegativeNumbers()
+		{
+			Assert.AreEqual(-15,calculator.AddTwoNumbers(-10,-5));
+		}
+
+		[Test]
+		public void TestCalculatorForPositiveNumbers()
+		{
+			Assert.AreEqual(15,calculator.AddTwoNumbers(10,5));
+		}
+
+		[SetUp]
+		public void SetUpCalculator()
+		{
+			calculator = new  SimpleCalculator();
+		}
+	}
+}
+
